d3ccefa1a91efbe74924a2de80e4dcbbe6b4d19f
Arpith Joseph <arpithjoseph2005@gmail.com>
2026-04-10 23:47:24 +0530

    Code add

--- src/DivisionHandling.java
+++ src/DivisionHandling.java
@@ -4,14 +4,15 @@ public class DivisionHandling {
         // TODO: Read two integers a and b
         // TODO: Enclose the division in a try block
         // TODO: Catch ArithmeticException
-        Scanner sc = new Scanner(System.in);
-        try {
-                int a = sc.nextInt();
-                int b = sc.nextInt();
-                System.out.println(a / b);
-        }catch (ArithmeticException e) {
-                System.out.println("Divide by zero error");
+        public static void main(String[] args){
+                Scanner sc = new Scanner(System.in);
+                try {
+                        int a = sc.nextInt();
+                        int b = sc.nextInt();
+                        System.out.println(a / b);
+                }catch (ArithmeticException e) {
+                        System.out.println("Divide by zero error");
+                }
+                sc.close();
         }
-        sc.close();
-        
 }
